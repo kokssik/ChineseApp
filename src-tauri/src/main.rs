d3107b65b5f41cf6bcd0c26b4b7c95c4dcbf56c1
kokssik <rudolf.sekac@gmail.com>
2023-05-06 23:56:46 +0200

    Added SQLite support, edited some HTML

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -5,6 +5,7 @@
 
 fn main() {
     tauri::Builder::default()
+        .plugin(tauri_plugin_sqlite::init())
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
--- src/routes/hanzi/+page.svelte
+++ src/routes/hanzi/+page.svelte
@@ -1,5 +1,20 @@
 <script>
   import { TabContent, TabPane } from "sveltestrap";
+  import SQLite from "tauri-plugin-sqlite-api";
+  import { onMount } from "svelte";
+  onMount(async () => {
+    console.log("Netušíš xD");
+
+    /** The path will be 'src-tauri/test.db', you can customize the path */
+    const db = await SQLite.open("./test.db");
+
+    /** execute SQL */
+    await db.execute(`
+      CREATE TABLE users (name TEST, age INTEGER);
+      INSERT INTO users VALUES ('Alice', 42);
+      INSERT INTO users VALUES ('Bob', 69);
+  `);
+  });
 </script>
 
 <TabContent>
@@ -24,3 +39,36 @@
     />
   </TabPane>
 </TabContent>
+
+<div class="card">
+  <div class="card-body">
+    <div class="row">
+      <div class="col-2">
+        <div class="divSymbol rounded border border-success">
+          <p class="symbol">我</p>
+        </div>
+      </div>
+      <div class="col-2">
+        <h2>Výslovnost:</h2>
+        <p>Wo3</p>
+      </div>
+      <div class="col-2">
+        <h2>Význam:</h2>
+        <p>Já</p>
+      </div>
+      <div class="col-6">One of three columns</div>
+    </div>
+  </div>
+</div>
+
+<style>
+  .symbol {
+    font-size: 75px;
+    text-align: center;
+  }
+  .divSymbol {
+    height: 100px;
+    width: 60%;
+    background-color: rgb(205, 249, 255);
+  }
+</style>
